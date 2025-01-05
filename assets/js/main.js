4fd8badfc89a73a59247359e77083914061204ef
MBNurbhanej72 <mbnurbhanej72@gmail.com>
2025-01-05 10:47:53 +0530

    Real Estate Section Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,17 @@ document.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar");
+  const navbarToggler = document.querySelector(".navbar-toggler");
+
+  navbarToggler.addEventListener("click", function () {
+    if (navbar.classList.contains("open")) {
+      navbar.classList.remove("open"); // Remove class if it exists
+    } else {
+      navbar.classList.add("open"); // Add class instantly on click
+    }
+  });
+});
